b203930d6661c21c0db8883d2298f0b7acfc135f
Yosept <yosept.flores@gmail.com>
2020-02-02 13:11:17 -0600

    shallow route for comments added

---
+++ app/controllers/r/comments_controller.rb
@@ -0,0 +1,7 @@
+module R
+  class CommentsController < ApplicationController
+    def show
+      @name = 'show'
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   namespace 'r' do
-    resources :leagueoflegends
+    scope shallow_path: "leagueoflegends" do
+      resources :leagueoflegends, shallow: true do
+        resources :comments
+      end
+    end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "home_page#index"
